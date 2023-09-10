5c82b4a3726487a763743c80aefbff550a435b3f
Bikathi Martin <bikathimartin@outlook.com>
2023-09-10 22:47:12 +0300

    Install prettier and pinia and format code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  css: ['@/assets/css/tailwind.css'],
-  modules: ['nuxt-icon']
-})
+	devtools: { enabled: true },
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	css: ['@/assets/css/tailwind.css'],
+	modules: ['nuxt-icon', '@pinia/nuxt'],
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./components/**/*.{js,vue}",
-		"./layouts/**/*.vue",
-		"./pages/**/*.vue",
-		"./plugins/**/*.{js}",
-		"./nuxt.config.{js,ts}",
+		'./components/**/*.{js,vue}',
+		'./layouts/**/*.vue',
+		'./pages/**/*.vue',
+		'./plugins/**/*.{js}',
+		'./nuxt.config.{js,ts}',
 	],
 	theme: {
 		extend: {},
