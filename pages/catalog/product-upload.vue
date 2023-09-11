d6e2cfef38fe0bdd595d30248c628246a9025e73
Bikathi Martin <bikathimartin@outlook.com>
2023-09-11 14:45:46 +0300

    Add parent route for the products catalog

---
+++ pages/catalog/product-upload.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="bg-red-500 flex-grow">
+		<h1>Hello world</h1>
+	</div>
+</template>
+
+<script setup>
+	definePageMeta({
+		layout: 'dashboard-layout',
+		name: 'product-upload',
+	});
+</script>
