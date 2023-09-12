9f7e6cfad07d74706a75d957b6e5c9e3ad417bf3
Bikathi Martin <bikathimartin@outlook.com>
2023-09-12 11:23:42 +0300

    Add page for managing product uploads to DB

--- pages/catalog/product-upload.vue
+++ pages/catalog/product-upload.vue
@@ -1,10 +1,15 @@
 <template>
-	<div class="bg-red-500 flex-grow">
-		<h1>Hello world</h1>
+	<div class="flex-grow flex flex-col items-center p-2">
+		<h1 class="mt-5 font-roboto text-2xl tracking-wide">
+			Record A New Product
+		</h1>
+		<ProductUploadForm />
 	</div>
 </template>
 
 <script setup>
+	import ProductUploadForm from '~/components/forms/ProductUploadForm.vue';
+
 	definePageMeta({
 		layout: 'dashboard-layout',
 		name: 'product-upload',
