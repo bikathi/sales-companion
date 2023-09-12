01ff68d54457f495a6b95bd773e5e3b1603d933f
Bikathi Martin <bikathimartin@outlook.com>
2023-09-12 11:24:22 +0300

    Create layout for in-dashboard view

--- layouts/dashboard-layout.vue
+++ layouts/dashboard-layout.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="h-screen min-h-screen flex flex-col">
-		<nav class="h-[8%] flex flex-row justify-center items-center px-2">
+		<nav class="h-[8%] flex flex-row justify-center items-center px-2 border-b">
 			<h1 class="self-center text-2xl">{{ organizationName }}</h1>
 			<div
 				class="w-14 h-14 border-2 rounded-full p-4 flex justify-center items-center hover:border-[4px] transition-all duration-200 ease-in-out hover:cursor-pointer">
