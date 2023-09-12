5d9c5aec017c877b85d5d83388859f47c32fded4
Bikathi Martin <bikathimartin@outlook.com>
2023-09-12 11:21:40 +0300

    Configure component scan

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	components: [
+		{ path: '~/components/forms' },
+		{ path: '~/components/icons' }
+	],
 	postcss: {
 		plugins: {
 			tailwindcss: {},
