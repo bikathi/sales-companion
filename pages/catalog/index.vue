6c72a27ccf52d5cb24f8eb98049db5039d656dbb
Bikathi Martin <bikathimartin@outlook.com>
2023-09-13 11:53:42 +0300

    Create route for catalog child route

---
+++ pages/catalog/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="flex-grow flex flex-col items-center p-2">
+		<h1>Default child route</h1>
+	</div>
+</template>
+
+<script setup>
+	definePageMeta({
+		layout: 'dashboard-layout',
+	});
+</script>
