686b4ab93424ab47d442f6420733e6e5b65c4714
Bikathi Martin <bikathimartin@outlook.com>
2023-09-15 23:13:24 +0300

    Modify UI of form to accomodate more fields

--- components/forms/ProductUploadForm.vue
+++ components/forms/ProductUploadForm.vue
@@ -2,7 +2,7 @@
 	<form class="h-fit p-2 w-[45%] space-y-2">
 		<div class="flex flex-col space-y-2">
 			<label
-				class="product-upload-labels"
+				class="product-upload-labels after:content-['*'] after:text-error"
 				for="product-id"
 				>Product Id(Auto Generated)</label
 			>
@@ -12,60 +12,92 @@
 				placeholder="Product Id"
 				id="product-id" />
 		</div>
+		<div class="flex space-x-4">
+			<div class="flex flex-col space-y-2 w-1/2">
+				<label
+					class="product-upload-labels after:content-['*'] after:text-error"
+					for="product-name"
+					>Product Name</label
+				>
+				<input
+					type="text"
+					class="product-upload-inputs"
+					placeholder="Product Name"
+					id="product-name" />
+			</div>
+			<div class="flex flex-col space-y-2 w-1/2">
+				<label
+					class="product-upload-labels"
+					for="product-id"
+					>Product Category</label
+				>
+				<select
+					id="product-category"
+					class="product-upload-inputs bg-transparent">
+					<option
+						class="hover:bg-gray-300 p-2 rounded-md"
+						v-for="(category, index) in [
+							'Baby Products',
+							'Beauty',
+							'Books',
+							'Cameras and Photos',
+							'Cellphones & Accessories',
+							'Music & DVD',
+						]"
+						:key="index"
+						:value="category">
+						{{ category }}
+					</option>
+				</select>
+			</div>
+		</div>
+		<div class="flex space-x-2">
+			<div class="flex flex-col space-y-2 w-1/2">
+				<label
+					class="product-upload-labels"
+					for="product-weight"
+					>Product Weight(KGs)</label
+				>
+				<input
+					type="text"
+					datatype="number"
+					pattern="[0-...]"
+					class="product-upload-inputs"
+					placeholder="Product Weight"
+					id="product-weight" />
+			</div>
+			<div class="flex flex-col space-y-2 w-1/2">
+				<label
+					class="product-upload-labels"
+					for="product-color"
+					>Product Color</label
+				>
+				<input
+					type="text"
+					datatype="number"
+					pattern="[0-...]"
+					class="product-upload-inputs"
+					placeholder="Product Color"
+					id="product-color" />
+			</div>
+		</div>
 		<div class="flex flex-col space-y-2">
 			<label
-				class="product-upload-labels"
-				for="product-name"
-				>Product Name</label
+				class="product-upload-labels after:content-['*'] after:text-error"
+				for="product-asignee"
+				>Product Asignee</label
 			>
 			<input
 				type="text"
 				class="product-upload-inputs"
-				placeholder="Product Name"
-				id="product-name" />
-		</div>
-		<div class="flex flex-col space-y-2">
-			<label
-				class="product-upload-labels"
-				for="product-id"
-				>Product Category</label
-			>
-			<select
-				id="product-category"
-				class="product-upload-inputs bg-transparent">
-				<option
-					class="hover:bg-gray-300 p-2 rounded-md"
-					v-for="(category, index) in [
-						'Baby Products',
-						'Beauty',
-						'Books',
-						'Cameras and Photos',
-						'Cellphones & Accessories',
-						'Music & DVD',
-					]"
-					:key="index"
-					:value="category">
-					{{ category }}
-				</option>
-			</select>
-		</div>
-		<div class="flex flex-col space-y-2">
-			<label
-				class="product-upload-labels"
-				for="product-weight"
-				>Product Weight(KGs)</label
-			>
-			<input
-				type="number"
-				class="product-upload-inputs"
-				placeholder="Product Weight"
-				id="product-weight" />
+				placeholder="Product Id"
+				id="product-asignee" />
 		</div>
 		<div class="w-full">
 			<label
 				for="photos"
-				class="product-upload-labels"
-				>Upload Company Logo</label
+				class="product-upload-labels after:content-['*'] after:text-error"
+				>Upload Product Photo</label
 			>
 			<label
 				for="dropzone-file"
@@ -85,7 +117,7 @@
 			</label>
 		</div>
 		<div class="flex justify-center p-2">
-			<button class="generic-button p-0">Upload Product</button>
+			<button class="generic-button p-2">Upload Product</button>
 		</div>
 	</form>
 </template>
