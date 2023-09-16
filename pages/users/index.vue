42e7ca28bfcd9a37701398c5f1dff358a2d96436
Bikathi Martin <bikathimartin@outlook.com>
2023-09-16 11:21:08 +0300

    Add entry for employee mamagement

---
+++ pages/users/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Welcome to users</h1>
+</template>
+
+<script setup>
+	definePageMeta({
+		layout: 'dashboard-layout',
+        name: "employee-list"
+	});
+</script>
---
+++ pages/users/user-creation.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Welcome to user creation</h1>
+</template>
+
+<script setup>
+	definePageMeta({
+		layout: 'dashboard-layout',
+		name: 'employee-creation',
+	});
+</script>
