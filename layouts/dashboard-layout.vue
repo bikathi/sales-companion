549ee3a5b92949f599bcf0d836d3d057783f0da5
Bikathi Martin <bikathimartin@outlook.com>
2023-09-21 23:03:17 +0300

    Mount alert system onto dashboard layout

--- layouts/dashboard-layout.vue
+++ layouts/dashboard-layout.vue
@@ -1,6 +1,11 @@
 <template>
-	<div class="h-screen min-h-screen flex flex-col">
-		<nav class="h-[8%] flex flex-row justify-center items-center px-2 border-b">
+	<div class="h-screen min-h-screen flex flex-col relative">
+		<Alert
+			v-if="displayAlert"
+			:alert-message="alertMessage"
+			:alert-type="alertType" />
+		<nav
+			class="h-[8%] flex flex-row justify-center items-center px-2 border-b">
 			<h1 class="self-center text-2xl">{{ organizationName }}</h1>
 			<div
 				class="w-14 h-14 border-2 rounded-full p-4 flex justify-center items-center hover:border-[4px] transition-all duration-200 ease-in-out hover:cursor-pointer">
@@ -17,4 +22,9 @@
 <script setup>
 	const runtimeConfig = useRuntimeConfig();
 	const organizationName = runtimeConfig.public.ORGANIZATION_NAME;
+
+	// alert properties
+	const displayAlert = ref(false);
+	const alertMessage = ref('Test Message Here');
+	const alertType = ref('info');
 </script>
