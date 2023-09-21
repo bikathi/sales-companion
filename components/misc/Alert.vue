e776df9b126ad2e288ea7f6e68bc311655a88607
Bikathi Martin <bikathimartin@outlook.com>
2023-09-21 22:53:13 +0300

    Create the alert component

--- components/misc/Alert.vue
+++ components/misc/Alert.vue
@@ -1,3 +1,19 @@
 <template>
-	<h1>App system alert</h1>
+	<div
+		class="flex bg-secondary/80 absolute bottom-4 left-4 text-white w-fit p-2 rounded-md items-center space-x-2">
+		<span class="font-nunito font">{{ alertMessage }}</span>
+		<div
+			class="radial-progress text-base-200"
+			style="--value: 80; --size: 1.5rem; --thickness: 4px"></div>
+	</div>
 </template>
+
+<script setup>
+	defineProps({
+		alertMessage: {
+			type: String,
+			default: 'Alert Message Here',
+			required: false,
+		},
+	});
+</script>
