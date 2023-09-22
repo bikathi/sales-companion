82a68fc3ad5dd9694e9235cb22e76a66fbf37905
Bikathi Martin <bikathimartin@outlook.com>
2023-09-22 21:24:28 +0300

    Complete timer countdown function

--- components/misc/Alert.vue
+++ components/misc/Alert.vue
@@ -1,11 +1,9 @@
 <template>
 	<div
 		:class="alertType === 'info' ? 'bg-secondary/80' : 'bg-alert'"
-		class="flex absolute bottom-4 left-4 text-white w-fit p-2 rounded-md items-center space-x-2">
+		class="flex absolute bottom-4 left-4 text-white w-fit p-2 rounded-md items-center space-x-2 shadow-md">
 		<span class="font-nunito font">{{ alertMessage }}</span>
-		<div
-			class="radial-progress text-base-200"
-			style="--value: 80; --size: 1.5rem; --thickness: 4px"></div>
+		<div class="radial-progress text-base-200 spinner-styles"></div>
 	</div>
 </template>
 
@@ -22,4 +20,25 @@
 			required: true,
 		},
 	});
+
+	const componentEmits = defineEmits(['closeAlert']);
+
+	const alertClosesAfter = ref(100);
+	onMounted(() => {
+		let alertHideCountdown = setInterval(function () {
+			alertClosesAfter.value -= 20;
+			if (alertClosesAfter.value === 0) {
+				componentEmits('closeAlert');
+				clearInterval(alertHideCountdown);
+			}
+		}, 1000);
+	});
 </script>
+
+<style scoped>
+	.spinner-styles {
+		--value: v-bind(alertClosesAfter);
+		--size: 1.5rem;
+		--thickness: 4px;
+	}
+</style>
